5d58ec9c2c222eaa96b3ae5ac74a38165e763d42
VincenzoIuliano <bubucane@outlook.it>
2024-11-05 15:35:13 +0100

    ADD: for to generate 10 random mails

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-console.log('Email List')
-    mailList = []
-    axios
-      .get('https://flynn.boolean.careers/exercises/api/random/mail')
-      .then((res) => {
-            let randomMail = res.data.response 
-            mailList.push(randomMail)
-        })
-      .catch((err) => {
-        console.log ('Abbiamo un errore' , err)
-      })  
-  
-console.log(mailList);
+console.log("Email List");
+mailList = [];
 
+for (let i = 0; i < 11; i++) {
+    axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((res) => {
+      let randomMail = res.data.response;
+      mailList.push(randomMail);
+    })
+    .catch((err) => {
+      console.log("Abbiamo un errore", err);
+    });
+  
+}
+
+console.log(mailList);
