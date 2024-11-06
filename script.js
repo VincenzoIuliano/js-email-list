3472448fb9dc16ae1dab77a15646854a29335e2d
VincenzoIuliano <bubucane@outlook.it>
2024-11-06 09:59:00 +0100

    ADD: live coding correction

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 console.log("Email List");
-mailList = [];
+// mailList = []; NON SERVE FARE ARRAY E RIEMPIRLO PERCHE A CAUSA DELLA ASINCRONICITA NON SO QUANDO SI RIEMPIE
 
-for (let i = 0; i < 11; i++) {
+const emailList = document.querySelector('.emails')
+
+
+for (let i = 0; i < 10; i++) {
     axios
     .get("https://flynn.boolean.careers/exercises/api/random/mail")
     .then((res) => {
-      let randomMail = res.data.response;
-      mailList.push(randomMail);
+      const randomMail = res.data.response;
+      // mailList.push(randomMail);
+
+      emailList.innerHTML += `<li>${randomMail}</li>`
     })
     .catch((err) => {
-      console.log("Abbiamo un errore", err);
-    });
-  
+      console.log("Abbiamo un errore:", err);
+    }); 
 }
 
-console.log(mailList);
+// console.log(mailList); NON SERVE FARE ARRAY E RIEMPIRLO PERCHE A CAUSA DELLA ASINCRONICITA NON SO QUANDO SI RIEMPIE
+function fetchmails (root, n = 10) {
+  for (let i = 0; i < 10; i++) {
+    axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((res) => {
+      const randomMail = res.data.response;
+      // mailList.push(randomMail);
+
+      root.innerHTML += `<li>${randomMail}</li>` // gli diamo dove inserire l'html senza usare parametri globali
+    })
+    .catch((err) => {
+      console.log("Abbiamo un errore:", err);
+    }); 
+}
+}
+
+
+fetchmails(emailList)
+loadMoreBtn.addEventListener('click', () => {
+  emailList.innerHTML = ''
+  fetchmails(emailList)
+})
